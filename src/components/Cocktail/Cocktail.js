4946165caf0699c6ae56c43c63796c7b6bc6cd0b
Christopher Baum <christopherabaum@gmail.com>
2023-04-19 14:33:57 -0500

    Initialize Cocktail and Quiz components

---
+++ src/components/Cocktail/Cocktail.js
@@ -0,0 +1,11 @@
+import './Cocktail.css';
+
+function Cocktail() {
+  return (
+    <div>
+      <h1>Im a cocktail</h1>
+    </div>
+  );
+}
+
+export default Cocktail;
---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,11 @@
+import './Quiz.css';
+
+function Quiz() {
+  return (
+    <div>
+      <h1>Im a Quiz</h1>
+    </div>
+  );
+}
+
+export default Quiz;
