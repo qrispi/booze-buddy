6e7cf135dab4fa010b70ce2f8db06e543f1c2ecf
Christopher Baum <christopherabaum@gmail.com>
2023-04-19 16:35:31 -0500

    Add function to format ingredients from fetched data

--- src/components/Cocktail/Cocktail.js
+++ src/components/Cocktail/Cocktail.js
@@ -3,6 +3,19 @@ import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 function Cocktail({cocktail}) {
 
+    const formatIngredients = () => {
+        const allKeys = Object.keys(cocktail);
+        const keys = allKeys.filter(key => key.includes('Ingredient'))
+        return keys.reduce((acc, key, index) => {
+            if(cocktail[key]) {
+                acc.push(cocktail['strMeasure' + (index + 1)] + cocktail[key])
+            }
+            return acc
+        }, [])
+    }
+
+    
+
   return (
     <div>
         <NavLink to="/">
