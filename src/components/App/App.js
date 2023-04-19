0aee6c7610441c28a277db8d11ece0ebbd5cf8e8
Christopher Baum <christopherabaum@gmail.com>
2023-04-19 15:44:57 -0500

    Add switch and redirect to prevent accidental URL navigation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,35 @@
 import './App.css';
 import Cocktail from '../Cocktail/Cocktail';
 import Quiz from '../Quiz/Quiz';
-import { Route, NavLink } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
 	<div>
-		<Route exact path="/">
-			<h1>Booze Buddy</h1>
-			<NavLink to="/cocktail">
-				<button>Surprise Me</button>
-			</NavLink>
-			<NavLink to="/quiz">
-				<button>Guide Me</button>
-			</NavLink>
-		</Route>
+		<Switch>
+			<Route exact path="/">
+				<h1>Booze Buddy</h1>
+				<NavLink to="/cocktail">
+					<button>Surprise Me</button>
+				</NavLink>
+				<NavLink to="/quiz">
+					<button>Guide Me</button>
+				</NavLink>
+			</Route>
 
-		<Route exact path="/cocktail">
-			<Cocktail />
-		</Route>
+			<Route exact path="/cocktail">
+				<Cocktail />
+			</Route>
 
-		<Route exact path="/quiz">
-			<Quiz />
-		</Route>
+			<Route exact path="/quiz">
+				<Quiz />
+			</Route>
+
+			<Route path="*">
+				<h2>ANY PATH</h2>
+				<Redirect from="*" to="/" />
+			</Route>
+		</Switch>
 	</div>
   );
 }
