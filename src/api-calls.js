4443b4579ed3ecc46a5968289a0308f55c23c53c
Christopher Baum <christopherabaum@gmail.com>
2023-04-21 20:54:41 -0500

    Update API key

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,6 +1,6 @@
 const getCocktails = async (path) => {
     try {
-        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/' + path);
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v2/9973533/' + path);
         return await response.json();
     } catch (error) {
         return error.message;
