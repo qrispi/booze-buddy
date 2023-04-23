6aebe1b878ded0462148236eac11339a35d0f8c6
Christopher Baum <christopherabaum@gmail.com>
2023-04-23 10:02:18 -0500

    Elevate Cocktail's state to App

--- src/components/Cocktail/Cocktail.js
+++ src/components/Cocktail/Cocktail.js
@@ -1,36 +1,16 @@
 import './Cocktail.css';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
-import { useState, useEffect } from 'react';
-import getCocktails from '../../api-calls';
 import listIngredients from '../../helper-functions';
 import cocktailImg from '../../images/cocktail.png'
 
-function Cocktail() {
-
-    const [cocktail, setCocktail] = useState({});
-	const [error, setError] = useState('');
-
-	const getRandomCocktail = () => {
-		const promise = getCocktails('random.php');
-        promise.then(data => {
-            if (typeof data === 'string' || data instanceof String) {
-                setError(data);
-            } else {
-                setCocktail(data.drinks[0]);
-            }
-        });
-	}
-
-	useEffect(() => {
-		getRandomCocktail();
-	}, [])
+function Cocktail({cocktail, getRandomCocktail}) {
 
   return (
     <>
         <header>
             <NavLink className='no-style' to="/">
                 <div className='logo'>
-                    <h1>Booze</h1> <img className="logo-img" src={cocktailImg} /> <h1>Buddy</h1>
+                    <h1>Booze</h1><img className="logo-img" src={cocktailImg} /><h1>Buddy</h1>
                 </div>
             </NavLink>
             <button onClick={getRandomCocktail}>Spin Again!</button>
