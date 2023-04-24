b9614c2ff46e76daf142d9ff35fb30b6a115361d
Christopher Baum <christopherabaum@gmail.com>
2023-04-23 20:55:34 -0500

    Remove extraneous code and intercept one other possibility for cypress

--- cypress/e2e/quiz-success.cy.js
+++ cypress/e2e/quiz-success.cy.js
@@ -4,6 +4,7 @@ describe('As a user, I should be able to answer three questions about my cocktai
         cy.intercept('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=Lime', {fixture: 'lime-results.json'});
         cy.intercept('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?g=Cocktail_glass', {fixture: 'glassware-results.json'});
         cy.intercept('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=14133', {fixture: 'full-quiz-cocktail.json'});
+        cy.intercept('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=17196', {fixture: 'full-quiz-cocktail.json'});
         cy.visit('http://localhost:3000/');
     });
     
