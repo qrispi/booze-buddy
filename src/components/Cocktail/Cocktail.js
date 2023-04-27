a4bfc09e92bbdfce48345b526c2a984f4f4859dc
Christopher Baum <christopherabaum@gmail.com>
2023-04-27 15:14:27 -0500

    Add useEffect to set loading state once component is mounted

--- src/components/Cocktail/Cocktail.js
+++ src/components/Cocktail/Cocktail.js
@@ -3,56 +3,60 @@ import { NavLink, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import listIngredients from '../../utilities';
 import cocktailImg from '../../images/cocktail.png';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Cocktail({cocktail, getRandomCocktail, error, clearError}) {
    
     const [loaded, setLoaded] = useState(false);
 
-  return (
-    <>
-        <Route exact path="/cocktail">
-            <header>
-                <NavLink className='no-style' to="/">
-                    <div className='logo'>
-                        <h1>Booze</h1><img className="logo-img" src={cocktailImg} alt='Cocktail Logo'/><h1>Buddy</h1>
-                    </div>
-                </NavLink>
-                <button onClick={() => {
-                    clearError();
-                    getRandomCocktail();
-                }}>Spin Again!</button>
-            </header>
-        </Route>
-        {error && 
-        <div className='error-container'>
-            <p>Bummer... We are experiencing server issues right now.</p>
-            <p>Please try again later!</p>
-        </div>
-        }
-        {!loaded &&
-            <img className="shaker-gif" src={require("../../images/shaker.gif")} alt="Shaking up a new cocktail!" /> 
-        }
-        {!error && loaded &&
-        <div className='cocktail-view'>
-            <div className='split-container'>
-                <h2>{cocktail.strDrink}</h2>
-                <img className="drink-img" src={cocktail.strDrinkThumb} alt={'Suggested Preparation of' + cocktail.strDrink}/>
+    useEffect(() => {
+        setTimeout(() => setLoaded(true), 500)
+    }, [loaded])
+
+    return (
+        <>
+            <Route exact path="/cocktail">
+                <header>
+                    <NavLink className='no-style' to="/">
+                        <div className='logo'>
+                            <h1>Booze</h1><img className="logo-img" src={cocktailImg} alt='Cocktail Logo'/><h1>Buddy</h1>
+                        </div>
+                    </NavLink>
+                    <button onClick={() => {
+                        clearError();
+                        getRandomCocktail();
+                    }}>Spin Again!</button>
+                </header>
+            </Route>
+            {error && 
+            <div className='error-container'>
+                <p>Bummer... We are experiencing server issues right now.</p>
+                <p>Please try again later!</p>
             </div>
-            <div className='split-container list-container'>
-                <h3>Directions:</h3>
-                <p>{cocktail.strInstructions}</p>
-                <h3>Ingredients:</h3>
-                <ul>
-                    {listIngredients(cocktail)}
-                </ul>
-                <h3>Glassware:</h3>
-                <p>{cocktail.strGlass}</p>
+            }
+            {!loaded &&
+                <img className="shaker-gif" src={require("../../images/shaker.gif")} alt="Shaking up a new cocktail!" /> 
+            }
+            {!error && loaded &&
+            <div className='cocktail-view'>
+                <div className='split-container'>
+                    <h2>{cocktail.strDrink}</h2>
+                    <img className="drink-img" src={cocktail.strDrinkThumb} alt={'Suggested Preparation of' + cocktail.strDrink}/>
+                </div>
+                <div className='split-container list-container'>
+                    <h3>Directions:</h3>
+                    <p>{cocktail.strInstructions}</p>
+                    <h3>Ingredients:</h3>
+                    <ul>
+                        {listIngredients(cocktail)}
+                    </ul>
+                    <h3>Glassware:</h3>
+                    <p>{cocktail.strGlass}</p>
+                </div>
             </div>
-        </div>
-        }
-    </>
-  );
+            }
+        </>
+    );
 }
 
 export default Cocktail;
